5d1ede12c4b86a155a9fb5950a94b5a98b707fef
Laxminarayan Sharma <laxminarayannsharma@gmail.com>
2023-05-23 22:45:54 +0530

    First git commit

---
+++ src/basic/FirstProgram.java
@@ -0,0 +1,8 @@
+package basic;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hey, This is my first program of java!");
+        System.out.println("I'm happy to run and push this code on git.");
+    }
+}
